1d07bf129d512504c1efd103105c30533c161bab
Apratap123 <anujrajput555.nr@gmail.com>
2026-05-14 19:32:46 +0530

    Remove wildcard route and use root route for production

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use('/api/objects', require('./routes/objectRoutes'));
 // Production Setup
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-  app.get('(.*)', (req, res) =>
+  app.get('/', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../../client', 'dist', 'index.html'))
   );
 } else {
